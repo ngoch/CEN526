0adb7d7e46005cfab8e9e49c39072e5d64d9c80f
Nikoloz Gochiashvili <nick@fina2.net>
2015-10-22 01:46:02 +0400

    Add swig layout

--- app.js
+++ app.js
@@ -11,15 +11,19 @@ MongoClient.connect('mongodb://localhost:27017/CEN526', function (err, db) {
     if (err) throw err;
 
     app.get('/', function (req, res) {
-        var name = 'CEN526';
-        return res.render('hello', {"name": name});
+        db.collection('lectures').find().limit(1).next(function (err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                return res.render('index', {'name': doc.name});
+            }
+        });
     });
 
-    app.get('*', function (req, res) {
-        return res.send(404).send('Page Not Found');
+    var server = app.listen(8000, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('Express server listening at http://%s:%s', host, port);
     });
-
-    app.listen(8000);
-
-    console.log('Express server started on port 80');
 });
