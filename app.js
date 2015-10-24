1136c239d6b79b9d7b78b674eb820dea77f2d343
Nikoloz Gochiashvili <nick@fina2.net>
2015-10-24 20:37:19 +0400

    Add initdb and lectures list

--- app.js
+++ app.js
@@ -20,6 +20,40 @@ MongoClient.connect('mongodb://localhost:27017/CEN526', function (err, db) {
         });
     });
 
+    app.get('/initdb', function (req, res) {
+        var lecturesCollection = db.collection('lectures');
+
+        lecturesCollection.drop(function () {
+            console.log('droped lectures collection');
+        });
+
+        var cen526 = {'name': 'CEN526', 'comment': 'Web Technologies Ph.D. Mikheil Rukhaia 19:00 - 22:00 B301'};
+
+        lecturesCollection.insertOne(cen526, function (err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('insert document');
+                console.log(doc.ops);
+            }
+            return res.render('initdb', {'result': doc.result.ok});
+        });
+    });
+
+    app.get('/lectures', function (req, res) {
+
+        var cursor = db.collection('lectures').find();
+
+        cursor.toArray(function (err, docs) {
+            console.log("retrieved records:");
+            console.log(docs);
+
+            return res.render('lectures', {'pagenam': 'Lectures list', 'lectures': docs});
+
+        });
+
+    });
+
     var server = app.listen(8000, function () {
         var host = server.address().address;
         var port = server.address().port;
